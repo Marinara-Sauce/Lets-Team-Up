674d33085b550c3a17ba47820868080e016c5b08
Dan Bliss <danbliss14027@gmail.com>
2022-03-05 15:13:42 -0500

    Created the server project

---
+++ server/src/main/java/com/letsteamup/api/letsteamupapi/LetsTeamUpApiApplication.java
@@ -0,0 +1,13 @@
+package com.letsteamup.api.letsteamupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsTeamUpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsTeamUpApiApplication.class, args);
+	}
+
+}
